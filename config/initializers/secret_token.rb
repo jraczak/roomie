9d98e5a0f7cb84e1c3451a370a1dbc3ee65e8847
Justin Raczak <jraczak@gmail.com>
2012-07-21 12:09:33 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roomie::Application.config.secret_token = '50e6adf629981c1b1dc0a2d65d730bf2964c19d7e28c6dd9fd297e55a5232c443126bf2a0ca7352b2e3a0577ba6fbf274537a89e56b7e36f7cfbbf89e3467b0f'
