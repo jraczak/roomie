fc14d46c09e31b976577b408da86ecdc39d80fa5
Justin Raczak <jraczak@gmail.com>
2012-10-09 19:41:53 -0400

    Remove remaining remnants of solr from Dwellings model

--- app/models/dwelling.rb
+++ app/models/dwelling.rb
@@ -1,11 +1,6 @@
 class Dwelling < ActiveRecord::Base
   attr_accessible :street_address, :city, :state, :zip, :nickname
   
-  searchable do
-    text :nickname
-    text :street_address
-  end
-  
   belongs_to :owner, :class_name => "User", :foreign_key => "owner_id"
   has_many :roomies, :class_name => "User"
   has_many :bills
@@ -14,11 +9,5 @@ class Dwelling < ActiveRecord::Base
   validates :city, presence: true
   validates :state, presence: true
   validates :zip, presence: true
-  
-   def self.search(q)
-     @search = Sunspot.search(Dwelling) do
-       fulltext q 
-     end
-   end
   
 end
