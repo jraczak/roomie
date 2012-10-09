0ba6c0d41cc310502bbbe444a9b27ae5fe8d8937
Justin Raczak <jraczak@gmail.com>
2012-10-09 13:04:08 -0400

    Add new Bill page and form partial. Add PAST DUE conditional to Bills on Dwelling view.

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -3,5 +3,20 @@ class BillsController < ApplicationController
   def show
     @bill = Bill.find(params[:id])
   end
+  
+  def new
+    @bill = Bill.new
+  end
+  
+  def create
+    @bill = Bill.create(params[:bill])
+    @bill.dwelling_id = current_user.dwelling.id
+    if @bill.save
+      flash[:success] = "Success! We've added your new bill."
+      redirect_to current_user.dwelling
+    else
+      render 'new'
+    end
+  end
 
 end
